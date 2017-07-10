eeb8380f02990553c379d95e6ed29f56095fb94a
anthyme <anthyme@gmail.com>
2017-07-10 15:55:30 +0200

    Add an AspNetCore version

---
+++ clients/c#/AspNetCore/AspNetCore/Kata.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore
+{
+    [Route("[controller]")]
+    public class OrderController : Controller
+    {
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Order value)
+        {
+            return Ok();
+            //return Ok(new { total = 19.03 });
+        }
+    }
+
+    [Route("[controller]")]
+    public class FeedbackController : Controller
+    {
+        [HttpPost]
+        public ActionResult Post()
+        {
+            return Ok();
+        }
+    }
+
+    public class Order
+    {
+        public decimal[] Prices { get; set; }
+        public int[] Quantities { get; set; }
+        public string Country { get; set; }
+        public string Reduction { get; set; }
+    }
+}
